fec7499f5b2ba637d80d56580e713898cc36aa51
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-03-29 18:21:36 +0200

    rework record_hrtf.py

--- head_tracking/sensor tracking/uno_orientation/uno_orientation.ino
+++ head_tracking/sensor tracking/uno_orientation/uno_orientation.ino
@@ -20,7 +20,7 @@ Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
 // pick your filter! slower == better quality output
 //Adafruit_NXPSensorFusion filter; // slowest
 //Adafruit_Madgwick filter;  // faster than NXP
-Adafruit_Mahony filter;  // fastest/smalleset
+//Adafruit_Mahony filter;  // fastest/smalleset
 
 #if defined(ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM)
   Adafruit_Sensor_Calibration_EEPROM cal;
@@ -34,8 +34,8 @@ Adafruit_Mahony filter;  // fastest/smalleset
 
 uint32_t timestamp;
 
-int analogPinAz = DAC0;     //initializing pin 
-int analogPinEle = DAC1;     //initializing pin 
+//int analogPinAz = DAC0;     //initializing pin 
+//int analogPinEle = DAC1;     //initializing pin 
 
 const int az_min_input = 0;
 const int az_max_input = 360;
@@ -50,7 +50,7 @@ float az = 0;
 float ele = 0;
 
 void setup() {
-  analogWriteResolution(12);
+//  analogWriteResolution(10);
   Serial.begin(115200);
   while (!Serial) yield();
   //if (!cal.begin()) {
@@ -58,27 +58,26 @@ void setup() {
   //} else if (! cal.loadCalibration()) {
   //  Serial.println("No calibration loaded/found");
   //}
-
-  cal.mag_hardiron[0] = -77.28; 
-  cal.mag_hardiron[1] = 27.33;
-  cal.mag_hardiron[2] = -12.08;
   
-  cal.mag_softiron[0] = 1.094;
-  cal.mag_softiron[1] = 0.037;
-  cal.mag_softiron[2] = 0.011;  
-  cal.mag_softiron[3] = 0.037;
-  cal.mag_softiron[4] = 0.945;
-  cal.mag_softiron[5] = 0.008;  
-  cal.mag_softiron[6] = 0.011;
-  cal.mag_softiron[7] = 0.008;
-  cal.mag_softiron[8] = 0.969;
+  cal.mag_hardiron[0] = -77.28; // 
+  cal.mag_hardiron[1] = 21.47;  // settles sec
+  cal.mag_hardiron[2] = -11.38; // 
+  
+  cal.mag_softiron[0] = 1.011;
+  cal.mag_softiron[1] = 0.121;
+  cal.mag_softiron[2] = -0.019;  
+  cal.mag_softiron[3] = 0.121;
+  cal.mag_softiron[4] = 1.053;
+  cal.mag_softiron[5] = -0.008;  
+  cal.mag_softiron[6] = -0.019;
+  cal.mag_softiron[7] = -0.008;
+  cal.mag_softiron[8] = 0.953;
   
   cal.mag_field = 35.51; 
-  
-  // in Radians/s
+
   cal.gyro_zerorate[0] = 0.075;
   cal.gyro_zerorate[1] = -0.019;
-  cal.gyro_zerorate[2] = -0.0672;
+  cal.gyro_zerorate[2] = -0.0672; 
 
   if (!init_sensors()) {
     Serial.println("Failed to find sensors");
@@ -90,10 +89,10 @@ void setup() {
   //magnetometer->printSensorDetails();
 
   setup_sensors();
-  //filter.begin(FILTER_UPDATE_RATE_HZ);
+  filter.begin(FILTER_UPDATE_RATE_HZ);
   timestamp = millis();
 
-  Wire.setClock(400000); // 400KHz
+  //Wire.setClock(400000); // 400KHz
 }
 
 
@@ -103,9 +102,9 @@ void loop() {
   float gx, gy, gz;
   static uint8_t counter = 0;
 
-  //if ((millis() - timestamp) < (1000 / FILTER_UPDATE_RATE_HZ)) {
-  //  return;
-  //}
+  if ((millis() - timestamp) < (1000 / FILTER_UPDATE_RATE_HZ)) {
+    return;
+  }
 
   timestamp = millis();
   // Read the motion sensors
@@ -161,11 +160,11 @@ void loop() {
   //Serial.print(", ");
 
   // write to analog pin
-  analogWriteResolution(12);
-  az = map(heading, az_min_input, az_max_input, min_output, max_output);
-  analogWrite(analogPinAz,az) ;     //setting  
-  ele = map(roll, ele_min_input, ele_max_input, min_output, max_output);
-  analogWrite(analogPinEle,ele) ;
+  //analogWriteResolution(12);
+  //az = map(heading, az_min_input, az_max_input, min_output, max_output);
+  //analogWrite(analogPinAz,az) ;     //setting  
+  //ele = map(roll, ele_min_input, ele_max_input, min_output, max_output);
+  //analogWrite(analogPinEle,ele) ;
   //Serial.print("az out: "); 
   //Serial.print(az);
   //Serial.print(", ele out: ");

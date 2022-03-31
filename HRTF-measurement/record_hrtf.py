a93aa077e8dfe4a3f696b1eda5fb214c5b8432ed
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-03-31 19:14:37 +0200

    work on record_hrtf.py:  add recordings of kemar and source_txt  add read_wav, create a slab.HRTF + write sofa

--- HRTF-measurement/record_hrtf.py
+++ HRTF-measurement/record_hrtf.py
@@ -30,38 +30,41 @@ fs = 48828  # sampling rate
 # rec_raw, rec_lvl, rec_full = freefield.test_equalization(speakers='all')
 # freefield.spectral_range(rec_full)
 
-# initialize setup
-freefield.initialize('dome', default='play_birec')
-#freefield.set_logger('WARNING')
-freefield.load_equalization(data_dir / 'dome_equalization_65')
 
-# generate probe signal
-slab.Signal.set_default_samplerate(fs)  # default samplerate for generating sounds, filters etc.
-signal = slab.Sound.chirp(duration=0.1, level=90, from_frequency=200, to_frequency=16000)
 
 def record_hrtfs(subject_id, repetitions, signal):
+    # initialize setup
+    freefield.initialize('dome', default='play_birec')
+    freefield.load_equalization(data_dir / 'dome_equalization_65')
+    # generate probe signal
+    slab.Signal.set_default_samplerate(fs)  # default samplerate for generating sounds, filters etc.
+    signal = slab.Sound.chirp(duration=0.1, level=90, from_frequency=200, to_frequency=16000)
     # get speaker coordinates
+    freefield.set_logger('WARNING')
     table_file = freefield.DIR / 'data' / 'tables' / Path(f'speakertable_dome.txt')
     source_locations = np.loadtxt(table_file, skiprows=1, usecols=(0, 3, 4), delimiter=",", dtype=float)
     speaker_ids = source_locations[:, 0].astype('int')
     # record
     n = 2  # record for n listener positions, 2 = front + back
     recordings = []
+    sources = deepcopy(source_locations)
+    print('Face fixpoint and press button to start recording.')
     for i in range(n):
-        print('face straight and press button to start recording')
         freefield.wait_for_button()
-        recordings = recordings + (dome_rec(signal, speaker_ids, source_locations, repetitions))
-        source_locations = rotate(source_locations, angle=int(360/n))  # rotate listener 180°
-    for bi_rec in recordings:
-        filename = 'in-ear_recordings\in-ear_%s_az%i_el%i.wav'%(subject_id, bi_rec[0], bi_rec[1])
+        recordings = recordings + (dome_rec(signal, speaker_ids, sources, repetitions))
+        if i < n:
+            sources = rotate(sources, angle=int(360/n))  # rotate listener 180°
+    for idx, bi_rec in enumerate(recordings):
+        filename = 'in-ear_recordings\in-ear_%s_src_idx%i_az%i_el%i.wav'%(subject_id,
+                    idx, bi_rec[0], bi_rec[1])
         bi_rec[2].write(data_dir / filename)
     np.savetxt(str(data_dir) + '\in-ear_recordings\sources_%s.txt'%(subject_id),
-               create_sources(source_locations, angle=180), fmt='%1.1f')
+               create_src_txt(recordings), fmt='%1.1f')
     freefield.set_logger('INFO')
     return recordings
 
 def dome_rec(signal, speaker_ids, source_locations, repetitions):
-    # record from various sound source locations
+    print('Recording from various sound source locations..')
     recordings = []  # list to store binaural recordings and source coordinates
     for speaker_id in speaker_ids:
         [speaker] = freefield.pick_speakers(speaker_id)
@@ -76,32 +79,45 @@ def dome_rec(signal, speaker_ids, source_locations, repetitions):
         elevation = source_locations[speaker.index, 2]
         rec = [azimuth, elevation, slab.Binaural(data=recs.mean(axis=0))]
         recordings.append(rec)
+        print('Progress: %i %%'%(speaker_id*2))
     return recordings
 
 def rotate(source_locations, angle):
     # rotate speaker coordinates by 90 degrees
     source_locations[:, 1] += angle
-    print('Rotate chair 90 degrees clockwise \nLook at fixpoint. Press button to start recording.')
+    print('Rotate chair %i degrees clockwise \nLook at fixpoint. Press button to start recording.'%angle)
     return source_locations
 
-def create_sources(source_locations, angle):
-    front_sources = source_locations
-    back_sources = deepcopy(source_locations)
-    back_sources[:, 1] += angle
-    back_sources[:, 0] += len(back_sources)
-    sources = np.vstack((front_sources, back_sources))
-    sources = np.c_[sources, np.ones(len(sources))*1.4]
+def create_src_txt(recordings):
+    sources = np.asarray(recordings)[:,:2]
+    sources = np.c_[np.arange(len(sources)), sources, np.ones(len(sources))*1.4].astype('float16')
     return sources
+
+def read_wav(path):
+    from natsort import natsorted
+    recordings = []  # list to hold slab.Binaural objects
+    path_list = []
+    for file_path in path.rglob('*.wav'):
+        path_list.append(str(file_path))
+    path_list = natsorted(path_list)
+    for file_path in path_list:
+        recordings.append(slab.Sound.read(file_path).data)
+    return slab.Sound(data=recordings)
 
 # def remove_mic_tf(recordings):
 #     mic_tf = ?
 #     recordings = recordings * mic_tf
+#
+# if __name__ == "__main__":
+#     recordings = record_hrtfs(subject_id='kemar_test', repetitions=5, signal=signal)
 
-if __name__ == "__main__":
-    recordings = record_hrtfs('kemar_test', repetitions=50, signal=signal)
-
-
-
+# write sofa
+slab.Signal.set_default_samplerate(fs)  # default samplerate for generating sounds, filters etc.
+signal = slab.Sound.chirp(duration=0.1, level=90, from_frequency=200, to_frequency=16000)
+recs = read_wav(path = data_dir / 'in-ear_recordings' / 'KEMAR')
+sources = np.loadtxt(data_dir / 'in-ear_recordings' / 'KEMAR' /'sources_KEMAR.txt')
+kemar_hrtf = slab.HRTF.estimate_hrtf(recs, signal, sources)
+kemar_hrtf.write_sofa(filename=data_dir / 'hrtfs' / 'KEMAR.sofa')
 
 
 """

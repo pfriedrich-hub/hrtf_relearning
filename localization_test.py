f8b40eb6c5dfe79a127695d3554b71fb6990588a
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-06-04 17:21:59 +0200

    add localization_test.py add pkl to test

--- localization_test.py
+++ localization_test.py
@@ -49,7 +49,7 @@ def localization_test():
     # loop over trials
     for index, speaker_id in enumerate(trial_sequence):
         trial_sequence.add_response(play_trial(speaker_id))  # play n trials
-    trial_sequence.save_json(data_dir / 'localization_data' / subj_id)
+    trial_sequence.save_pickle(data_dir / 'localization_data' / subj_id)
     freefield.halt()
     for cam in cams:
         if cam.IsInitialized():
@@ -89,5 +89,10 @@ if __name__ == "__main__":
     trialsequence = localization_test()
 
 """
-trial_sequence.load_json(file_name=data_dir/data_dir / 'localization_data' / subj_id)
+from pathlib import Path
+data_dir = Path.cwd() / 'data'
+subj_id = '001'
+sequence = slab.Trialsequence(conditions=47, n_reps=1)
+sequence.load_pickle(file_name=data_dir/data_dir / 'localization_data' / subj_id)
+data = sequence.data
 """

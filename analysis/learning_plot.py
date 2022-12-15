cbb5a072a42c07779373655aee56a5e018dbf5d8
paulfriedrich <paul.friedr1@gmail.com>
2022-12-15 22:17:51 +0100

    work on plots

--- analysis/learning_plot.py
+++ analysis/learning_plot.py
@@ -4,8 +4,8 @@ import numpy
 import slab
 from matplotlib import pyplot as plt
 import scipy
-""" -------  plot group averaged learning curve ------ """
 
+""" -------  plot group averaged learning curve ------ """
 
 exclude = []
 w2_exclude = ['cs', 'lw']
@@ -28,7 +28,9 @@ for condition in conditions:
 
 ex_idx = [subjects.index(ex) for ex in w2_exclude]  # remove w2 excludes
 loc_dict['earmolds_1']['data'] = numpy.delete(loc_dict['earmolds_1']['data'], ex_idx, axis=0)
-days = numpy.arange(1, 13) # days of measurement
+loc_dict['ears_free']['data'] = numpy.delete(loc_dict['ears_free']['data'], ex_idx, axis=0)
+
+days = numpy.arange(1, 13)  # days of measurement
 days[-1] = 16
 ef = numpy.mean(loc_dict['ears_free']['data'], axis=0)  # means
 m1 = numpy.mean(loc_dict['earmolds']['data'], axis=0)
@@ -69,83 +71,10 @@ axes[0].set_ylabel('Elevation Gain')
 axes[1].set_xlabel('Days')
 axes[1].set_ylabel('Elevation in degrees')
 axes[1].legend()
+
+fig.text(0.3, 0.8, f'n={len(subjects)}', ha='center', size=10)
+fig.text(0.5, 0.8, f'n={len(subjects) - len(w2_exclude)}', ha='center', size=10)
+subjects = ' '.join([str(item) for item in subjects])
+w2_exclude = ' '.join([str(item) for item in w2_exclude])
+plt.suptitle(f'w1: {subjects}, w2 excluded: {w2_exclude}')
 plt.show()
-
-
-"""
-# RMSE & SD
-# week 1
- # week 1 learning curve
-colors = ['k', '0.6']
-labels = ['RMSE', 'SD']
-for i in range(1, 3):
-    ax[1].plot(w1[:2], numpy.mean(w1_data[:, i, :2], axis=0), c=colors[i-1], ls=(0, (5, 10)), lw=0.8)  # day one mold insertion
-    ax[1].plot(w1[1:-1], numpy.mean(w1_data[:, i, 1:-1], axis=0), c=colors[i-1], linewidth=1.5, label=labels[i-1])  # week 1 learning curve
-    ax[1].plot(w1[-2:], numpy.mean(w1_data[:, i, -2:], axis=0), c=colors[i-1], ls=(0, (5, 10)), lw=0.8)  # week 1 mold removal
-    # week 2
-    ax[1].plot(w2[:2], numpy.mean(w2_data[:, i, :2], axis=0), c=colors[i-1], ls=(0, (5, 10)), lw=0.8)  # week 2 mold insertion
-    ax[1].plot(w2[1:-3], numpy.mean(w2_data[:, i, 1:-3], axis=0), c=colors[i-1], linewidth=1.5)  # week 2 learning curve
-    ax[1].plot(w2[-3:-1], numpy.mean(w2_data[:, i, -3:-1], axis=0), c=colors[i-1], ls=(0, (5, 10)), lw=0.8)  # week 2 mold removal
-    # mold 1 adaptation persistence
-    ax[1].plot([w1[-1], w2[-2]], [numpy.mean(w1_data[:, i, -2], axis=0), numpy.mean(w2_data[:, i, -2], axis=0)],
-               c=colors[i-1], ls=(0, (5, 10)), lw=0.8)
-    # mold 2 adaptation persistence
-    ax[1].plot(w2[-2:], numpy.mean(w2_data[:, i, -2:], axis=0), c=colors[i-1], ls=(0, (5, 10)), lw=0.8)
-    # error bars
-    ax[1].errorbar(w1[0], numpy.mean(w1_data[:, i, 0], axis=0), capsize=3, yerr=w1_err[i, 0],
-                   fmt="o", c=colors[i-1], elinewidth=0.8, markersize=5, fillstyle='none')
-    ax[1].errorbar(w1[1:-1], numpy.mean(w1_data[:, i, 1:-1], axis=0), capsize=3, yerr=w1_err[i, 1:-1],
-                   fmt="o", c=colors[i-1], elinewidth=0.8, markersize=5)
-    ax[1].errorbar(w1[-1], numpy.mean(w1_data[:, i, -1], axis=0), capsize=3, yerr=w1_err[i, -1],
-                   fmt="o", c=colors[i-1], elinewidth=0.8, markersize=5, fillstyle='none')
-    ax[1].errorbar(w2[1:-1], numpy.mean(w2_data[:, i, 1:-1], axis=0), capsize=3, yerr=w2_err[i, 1:-1],
-                   fmt="o", c=colors[i-1], elinewidth=0.8, markersize=5)
-    ax[1].errorbar(w2[-1:], numpy.mean(w2_data[:, i, -1], axis=0), capsize=3, yerr=w2_err[i, -1],
-                   fmt="o", c=colors[i-1], elinewidth=0.8, markersize=5)
-
-
-
-
-
-
-
-# plot Elevation Gain, RMSE and response variability across experiment
-
-n = 17  # number of measurements per participant
-subject_paths = list((Path.cwd() / 'data' / 'experiment' / bracket).iterdir())
-condition_list = ['ears_free', 'earmolds', 'earmolds_1']
-subj_list = []
-data = numpy.zeros((len(subject_paths), 3, n))  # subject_id x measurement (eg, rmse, sd) x n datapoints
-for subj_idx, subject_path in enumerate(subject_paths):
-    # print('\n' + subject_path.name)
-    subj_list.append(subject_path.name)
-    for condition in condition_list:
-        # print(condition)
-        subject_dir = subject_path / condition
-        f_idx = 0
-        for file_name in sorted(list(subject_dir.iterdir())):
-            if file_name.is_file() and not file_name.suffix == '.sofa':
-                # print(file_name.name)
-                sequence = slab.Trialsequence(conditions=45, n_reps=1)
-                sequence.load_pickle(file_name=file_name)
-                g, r, s = localization.localization_accuracy(sequence, show=False)
-                if condition == 'ears_free' and f_idx == 0:
-                    n_idx = 0
-                elif condition == 'earmolds':
-                    n_idx = f_idx + 1
-                elif condition == 'ears_free' and f_idx == 1:
-                    n_idx = 7  # 7th datapoint (ears free in between the two molds)
-                elif condition == 'earmolds_1':
-                    n_idx = f_idx + 8
-                data[subj_idx, 0, n_idx], data[subj_idx, 1, n_idx], data[subj_idx, 2, n_idx] = g, r, s
-                f_idx += 1
-
-                w1_data = data[:, :, :len(w1)]
-                excl_idx = [subj_list.index(excl) for excl in w2_exclude]
-                w2_data = numpy.delete(data[:, :, -len(w2):], excl_idx, axis=0)  # exclude participants from week 2
-                # exclude a participant from week 1 here:
-                w1_data = numpy.delete(w1_data, excl_idx, axis=0)
-
-                # standard mean error (sd / sqrt(n)): (sd = sqrt(var), var = mean distance from sample mean)
-                w1_err, w2_err = scipy.stats.sem(w1_data, axis=0), scipy.stats.sem(w2_data, axis=0)
-"""
--- analysis/localization_plot.py
+++ analysis/localization_plot.py
@@ -1,10 +1,11 @@
 import analysis.localization_analysis as localization
 from pathlib import Path
 import matplotlib.pyplot as plt
+import numpy
 
 """ -------  plot localization accuracy of all participants --------- """
 
-plot_condition = 'earmolds'
+plot_condition = 'earmolds_1'
 exclude = []
 w2_exclude = []
 bracket = 'bracket_1'
@@ -24,5 +25,60 @@ for s_id, subj in enumerate(subjects):
 fig.text(0.5, 0.07, 'Response azimuth (deg)', ha='center')
 fig.text(0.08, 0.5, 'Response elevation (deg)', va='center', rotation='vertical')
 axis[0, 0].set_xticks(axis[0, 0].get_xticks().astype('int'))
-for idx, i in enumerate(range(2, 10, 2)):
-    fig.text(i/10, 0.95, subjects[idx])
+for sub_idx, i in enumerate(range(2, 10, 2)):
+    fig.text(i/10, 0.95, subjects[sub_idx])
+
+
+
+
+""" -------  plot localization free, 1st vs last day of molds --------- """
+exclude = []
+w2_exclude = []
+bracket = 'bracket_1'
+
+conditions = ['ears_free', 'earmolds', 'earmolds_1']
+path = Path.cwd() / 'data' / 'experiment' / 'bracket_1'
+loc_dict = localization.get_localization_data(path, conditions)
+subjects = list(loc_dict['ears_free'].keys())
+for ex in exclude: subjects.remove(ex)
+
+fig, axis = plt.subplots(6, len(subjects), sharex=True, sharey=True, figsize=[15, 10])
+fig.subplots_adjust(left=None, bottom=0.1, right=0.96, top=0.96, wspace=0.05, hspace=0.1)
+
+for s_id, subj in enumerate(subjects):
+    ax = axis[0, s_id]
+    localization.localization_accuracy(loc_dict['ears_free'][subj][0], show=True,
+                                       plot_dim=2, binned=True, axis=ax)  # ears free D1
+    eg = ax.get_title()[-4:]
+    ax.set_title(label=f'EG {eg}', y=0.8, size=10)
+    for ax_id, i in zip([0, 1], [0, 5]):
+        ax = axis[ax_id+1, s_id]
+        localization.localization_accuracy(loc_dict['earmolds'][subj][i], show=True,
+                                           plot_dim=2, binned=True, axis=ax)  # M1 D1 / D6
+        eg = ax.get_title()[-4:]
+        ax.set_title(label=f'EG {eg}', y=0.8, size=10)
+    if len(loc_dict['earmolds_1'][subj]) >= 5:
+        for ax_id, i in zip([1, 2], [0, 5]):
+            ax = axis[ax_id+2, s_id]
+            localization.localization_accuracy(loc_dict['earmolds_1'][subj][i], show=True,
+                                               plot_dim=2, binned=True, axis=ax)  # M2 D1 / D6
+            eg = ax.get_title()[-4:]
+            ax.set_title(label=f'EG {eg}', y=0.8, size=10)
+        localization.localization_accuracy(loc_dict['earmolds'][subj][-1], show=True,
+                                           plot_dim=2, binned=True, axis=axis[5, s_id])  # M1 D11
+        eg = axis[5, s_id].get_title()[-4:]
+        axis[5, s_id].set_title(label=f'EG {eg}', y=0.8, size=10)
+    # for ax_id, i in zip([0, 1], [0, 5]):
+    #     localization.localization_accuracy(loc_dict['earmolds'][subj][i], show=True,
+    #                                        plot_dim=2, binned=True, axis=axis[ax_id+1, s_id])  # M2 D16
+fig.text(0.5, 0.05, 'Response azimuth (deg)', ha='center', size=12)
+fig.text(0.08, 0.5, 'Response elevation (deg)', va='center', rotation='vertical', size=12)
+axis[0, 0].set_xticks(axis[0, 0].get_xticks().astype('int'))
+yticks = axis[0, 0].get_yticks()#[1:-1]
+axis[0, 0].set_yticks(yticks.astype('int'))
+# for idx, i in enumerate(range(2, 10, 2)):
+#     fig.text(i/10, 0.95, subjects[idx])
+c = ['Ears Free\nDay1', 'Mold 1\nDay 1', 'Mold 1\nDay 6', 'Mold 2\nDay 1', 'Mold 2\nDay 6', 'Mold 1\nDay 11']
+for idx, i in enumerate(numpy.linspace(0.87, 0.15, 6)):
+    fig.text(0.03, i, f'{c[idx]}', size=13)
+

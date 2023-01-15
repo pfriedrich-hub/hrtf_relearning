b366dba73bce99dc79753dbf61ede9c3dfc3cc91
pfriedrich-hub <61830224+mxschlz@users.noreply.github.com>
2023-01-15 10:55:59 +0100

    add experiment data

--- localization_test.py
+++ localization_test.py
@@ -10,7 +10,7 @@ import head_tracking.meta_motion.mm_pose as motion_sensor
 fs = 48828
 slab.set_default_samplerate(fs)
 
-subject_id = 'test'
+subject_id = 'vk'
 condition = 'Earmolds Week 1'
 data_dir = Path.cwd() / 'data' / 'experiment' / 'bracket_2' / subject_id / condition
 

fe3c71e40666896f14376dad32b956c3eb6de793
paulfriedrich <paul.friedr1@gmail.com>
2025-02-18 14:53:19 +0100

    clean up sound folder

--- data/sounds/generate_sounds.py
+++ data/sounds/generate_sounds.py
@@ -3,5 +3,8 @@ slab.set_default_samplerate(48828)
 from pathlib import Path
 data_path = Path.cwd() / 'data' / 'sounds'
 
-noise = slab.Sound.pinknoise(duration=5.0)
-noise.write(data_path / 'pinknoise_pulses.wav')
+# training stimulus
+noise = slab.Sound.pinknoise(duration=60.0)
+noise.write(data_path / 'training_noise.wav')
+
+# test stimulus

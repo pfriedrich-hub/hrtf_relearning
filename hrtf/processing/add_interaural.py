e5f9d195fbe1906416ff32baf0567d4e6b17ecf7
paulfriedrich <paul.friedr1@gmail.com>
2025-05-14 13:35:28 +0200

    add ils

--- hrtf/processing/add_interaural.py
+++ hrtf/processing/add_interaural.py
@@ -2,29 +2,49 @@
 import slab
 import numpy
 import copy
+import scipy
 
-def add_ild(hrtf):
-    """
-    Add interaural level difference to HRTF (works on TF data)
-    """
-    w = hrtf[0].frequencies
-    for source_idx in range(hrtf.n_sources):
-        coordinates = hrtf.sources.vertical_polar[source_idx]
-        ils_db = slab.Binaural.azimuth_to_ild(coordinates[0], frequency=2000)
-        # Convert dB gains to linear scale
-        ils = 10 ** (ils_db / 20)
-        h = hrtf[source_idx].data
-        # H = ...  # existing frequency response (complex)
-        # G_dB = ...  # desired gain in dB
-        # G = 10 ** (G_dB / 20)
-        #
-        # H_mod = H * G
-    return hrtf
+def add_ild(hrtf, template_hrtf=None, band_stop=(6e3, 11e3)):
+        """
+        Get a low-res version of a DTF (at 0° az and 0° elevation) from a recorded HRTF to externalize a synthetic HRTF
+        """
+        print('Adding interaural level spectrum for each azimuth.')
+        n_bins = hrtf[0].n_samples
+        if not template_hrtf:
+            template_hrtf = slab.HRTF.kemar()  # load KEMAR as default
+        for azimuth in numpy.unique(hrtf.sources.vertical_polar[:, 0]):
+            # todo workaround: convert to psychophys. convention to use with kemar
+            if azimuth > 180:
+                azimuth = azimuth - 360
+            # get IR data of the template dtf (at spec. az and 0° elevation)
+            template_dtf_idx = template_hrtf.get_source_idx(azimuth, 0)[0]
+            ir_data = template_hrtf.data[template_dtf_idx].data
+            # get low-res version of HRTF spectrum
+            w, tf_data_l = numpy.abs(scipy.signal.freqz(ir_data[:, 0], worN=12, fs=hrtf.samplerate))
+            _, tf_data_r = numpy.abs(scipy.signal.freqz(ir_data[:, 1], worN=12, fs=hrtf.samplerate))
+            tf_data = numpy.vstack((tf_data_l, tf_data_r))
+            tf_data[:, 0] = 1  # avoids low-freq attenuation in KEMAR HRTF
+            tf_data = scipy.signal.resample(tf_data, n_bins, axis=1)  # resample to hrtf samplerate
+            w = numpy.linspace(0, w.max(), n_bins)  # frequency bins
+            # avoid interference of interaural level spectrum with band stop region
+            tf_data[:, numpy.where((w > band_stop[0]) & (w < band_stop[1]))] = numpy.finfo(float).eps
+            # ramp edges of bandstop region
+            envelope = lambda t: numpy.sin(numpy.pi * t / 2) ** 2  # squared sine window
+            multiplier = envelope(numpy.linspace(0.0, 1.0, 20))
+            tf_data[:, numpy.where(w < band_stop[0])[0][-20:]] *= numpy.flip(multiplier)
+            tf_data[:, numpy.where(w > band_stop[1])[0][:20]] *= multiplier
+            hrtf_idx = hrtf.get_source_idx(azimuth=azimuth).tolist()
+            for id in hrtf_idx:
+                # todo workaround: mirror az to use with kemar
+                hrtf[id].data[:, 0] += tf_data[1]
+                hrtf[id].data[:, 1] += tf_data[0]
+        return hrtf
 
 def add_itd(hrir):
     """
     Add interaural time difference to HRIR (only works on IR)
     """
+    print('Adding interaural time differences for each azimuth.')
     out = copy.deepcopy(hrir)
     for source_idx in range(hrir.n_sources):
         coordinates = hrir.sources.vertical_polar[source_idx]
